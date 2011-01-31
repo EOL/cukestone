4f605d8a501d46e7ab4477dbf5142443edb67fb0
Tyler Chamberland <tyler.james.chamberland@gmail.com>
2011-01-31 12:31:43 -0500

    half-way there for sane paths/selectors

--- config/environment.rb
+++ config/environment.rb
@@ -7,3 +7,16 @@ module Cukestone
   Conf = OpenStruct.new(conf_dict)
 end
 
+#self-test methods can go here
+#this is not executed if this is not
+#the top level script
+if __FILE__ == $0 then
+  puts "Here are the paths available to Cukestone"
+  p Cukestone::Conf.paths
+  
+  puts
+  
+  puts "Here are the selectors available to Cukestone"
+  p Cukestone::Conf.selectors
+end
+
--- features/support/paths.rb
+++ features/support/paths.rb
@@ -6,25 +6,19 @@ module NavigationHelpers
   # step definition in web_steps.rb
   #
   def path_to(page_name)
-    case page_name
-
-    when /the home\s?page/
-      '/'
-
-    when /preferences/
-     '/settings?return_to=/'
-     
-    when /the Login page/
-       '/login?return_to=/'
-
-    else
+    #match given the page name with the first that matches a key from
+    #config/config.yml collections then return the associated
+    #path value from the configuration
+    if not Cukestone::Conf.paths.keys.detect {| path | Regexp.new(path).match(page_name)}.nil? then
+      Cukestone::Conf.paths[$&] 
+    else 
       begin
         page_name =~ /the (.*) page/
         path_components = $1.split(/\s+/)
         self.send(path_components.push('path').join('_').to_sym)
       rescue Object => e
         raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
-          "Now, go and add a mapping in #{__FILE__}"
+          "Now, go and add a mapping in config/config.yml"
       end
     end
   end
