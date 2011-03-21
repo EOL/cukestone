c3746c4b899e3f91cc9783d3c857cd1bea587b39
Feng Sun <frank.fengsun@gmail.com>
2011-03-21 17:35:18 -0400

    steps not needed for this feature files are removed.

--- features/step_definitions/signin_steps.rb
+++ features/step_definitions/signin_steps.rb
@@ -22,42 +22,9 @@ Given /^I fill in (.+) with the "([^"]*)"$/ do |field,password|
 end
 
 
-When /^(?:|I )follow ([^"]*)(?: within ([^"]*))?$/ do |link, selector|
-  click_link(to_selector(link))
-end
-
-When /^(?:|I )follow the link with text "([^"]*)" (within (.+))$/ do |link,selector|
-  with_scope(to_selector(selector)) do
-    find_link(link).click
-  end
-end
 
 
-Then /^(?:|I )should see "([^"]*)" within (.+)$/ do |text, selector|	
-  with_scope(to_selector(selector)) do
-    if page.respond_to? :should
-      page.should have_content(text)
-    else
-      assert page.has_content?(text)
-    end
-  end
-end
 
-When /^(?:|I )should see a link with text "([^"]*)" (within (.+))$/ do |text,selector|
-  with_scope(to_selector(selector)) do
-    page.should have_link(text)		
-  end	  
-end
-
-Then /^(?:|I )should not see a link with text "([^"]*)"$/ do |text|
-  page.should_not have_link(text)
-end
-
-Then /^(?:|I )should not see a link with text "([^"]*)" within (.+)$/ do |text,selector|
-  with_scope(to_selector(selector)) do
-    page.should_not have_link(text)		
-  end
-end
 
 
 
