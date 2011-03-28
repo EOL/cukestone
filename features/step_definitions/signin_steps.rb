c3602b251a61e56435726f1fe4640e23e9fada98
Feng Sun <frank.fengsun@gmail.com>
2011-03-28 12:59:33 -0400

    fix the password visible problem

--- features/step_definitions/signin_steps.rb
+++ features/step_definitions/signin_steps.rb
@@ -7,12 +7,22 @@ Then /^(.+) should be invisible$/ do |field|
   find(to_selector(field)).visible?.should be_false
 end
 
-Given /^I sign in as "(.*)"$/ do |user|
+When /^I login as a user$/ do
+    fill_in(to_selector('the username field'), :with => Cukestone::Conf.user)
+    fill_in(to_selector('the password field'), :with => Cukestone::Conf.user_password)
+    find(to_selector('the login button')).click
+end
+
+When /^I login as a curator$/ do
+    fill_in(to_selector('the username field'), :with => Cukestone::Conf.curator)
+    fill_in(to_selector('the password field'), :with => Cukestone::Conf.curator_password)
+    find(to_selector('the login button')).click
+end
+
+Given /^I sign in as a (.*)$/ do |user|
   steps %Q{
     When I am on the login page
-    And I fill in the username field with "#{user}"
-    And I fill in the password field with the "#{user}_password"
-    And I press the login button
+    And I login as a #{user}
     And wait 3 seconds
   }  
 end
